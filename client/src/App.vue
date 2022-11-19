96da53138d21abab40b322d679b0b984c5dd51fb
정수진 <sssjsjj@ipartners.co.kr>
2022-11-19 16:48:10 +0900

    client side 프로젝트 생성 후 article 가져와 화면에 뿌리기

---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <ul>
+      <li
+        v-for="article in articles"
+        :key="article.id"
+      >
+        {{ article.content }}
+        {{ article.createdAt }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      articles: null,
+      newContent: ''
+    }
+  },
+  created() {
+    this.getArticles()
+  },
+  methods: {
+    async getArticles() {
+      const { data } = await axios.get('http://localhost:3009/read')
+      this.articles = data
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
