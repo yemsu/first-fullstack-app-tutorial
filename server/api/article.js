aaa4f8614c8a4b562c2f680bb3e072b4f34009c7
정수진 <sssjsjj@ipartners.co.kr>
2022-11-19 16:31:21 +0900

    model 구조 분해 할당으로 가독성 개선

--- server/api/article.js
+++ server/api/article.js
@@ -1,9 +1,9 @@
-const model = require('../mongoose/model')
+const { Article } = require('../mongoose/model')
 
 // CREATE
 const articleCreate = async (req, res) => {
   const { content } = req.body
-  const newArticle = await model.Article({ content })
+  const newArticle = await Article({ content })
   const saveRequest = await newArticle.save()
   console.log(saveRequest)
   res.send(saveRequest)
@@ -11,14 +11,14 @@ const articleCreate = async (req, res) => {
 
 // READ
 const articleRead = async (req, res) => {
-  const articles = await model.Article.find({})
+  const articles = await Article.find({})
   res.send(articles)
 }
 
 // UPDATE
 const articleUpdate = async (req, res) => {
   const { id, content } = req.body
-  const updatedArticle = await model.Article.findByIdAndUpdate(id, { content }, {
+  const updatedArticle = await Article.findByIdAndUpdate(id, { content }, {
     returnDocument: 'after' // 변경된 값 리턴하도록 옵션 설정.
   })
   res.send(updatedArticle) 
@@ -27,7 +27,7 @@ const articleUpdate = async (req, res) => {
 // DELETE
 const articleDelete = async (req, res) => {
   const { id } = req.params
-  const deleteArticle = await model.Article.findByIdAndDelete(id)
+  const deleteArticle = await Article.findByIdAndDelete(id)
   res.send(deleteArticle)
 }
 
